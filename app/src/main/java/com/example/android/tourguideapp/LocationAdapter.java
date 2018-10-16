5af8e21ffc23f3dee8a457113f16488c1b25301e
#victor <victormba34@gmail.com>
2018-10-16 15:15:11 -0500

    Error free version

--- app/src/main/java/com/example/android/tourguideapp/LocationAdapter.java
+++ app/src/main/java/com/example/android/tourguideapp/LocationAdapter.java
@@ -10,6 +10,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.List;
 
 public class LocationAdapter extends ArrayAdapter<Location> {
